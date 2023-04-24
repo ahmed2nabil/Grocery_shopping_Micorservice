a316f91076a42630e50cd43299e36201d189d224
ahmed2nabil <ahmednabl992@gmail.com>
2023-04-24 15:24:27 +0200

    added test Cases Configuration

---
+++ products/src/services/product-service.test.js
@@ -0,0 +1,9 @@
+describe("ProductService", () => {
+
+    describe("CreateProduct", () => {
+        
+      test("validate user inputs", () => {});
+  
+      test("Validate response", async () => {});
+    });
+  });
---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,10 @@
+describe("ShoppingService", () => {
+
+    describe("PlaceOrder", () => {
+
+        test("validate user inputs", () => {});
+  
+      test("Validate response", async () => {});
+    });
+  });
+  
